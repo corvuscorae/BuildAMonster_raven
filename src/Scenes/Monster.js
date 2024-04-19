12b8846dc03b4e2f06ebbe19780dff50a9baeac7
corvuscorae <corvuscorae@gmail.com>
2024-04-18 17:46:41 -0700

    v1

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -31,9 +31,46 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        /*
+        new sprite -> x = (sprite.width / 2) + this. bodyX // how to access sprite's data? (width, height, etc)
+        console.log(monsterParts["arm_blueA.png"].width); <- nope
+        console.log(my.atlasXML["arm_blueA.png"].width); <- nope
+        */
+       
 
-        
+        // body
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        // eye
+        /*my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "eye_human.png");
+        my.sprite.eye.scaleX = 1.5; my.sprite.eye.scaleY = 1.5;*/
+        // arms
+        let arm = "arm_greenB.png";
+        my.sprite.rightArm = this.add.sprite(this.bodyX + 90, this.bodyY + 40, "monsterParts", arm); 
+        my.sprite.leftArm = this.add.sprite(this.bodyX - 90, this.bodyY + 40, "monsterParts", arm);
+        my.sprite.leftArm.flipX = true;
+        // legs
+        let leg = "leg_greenC.png";
+        my.sprite.rightLeg = this.add.sprite(this.bodyX + 70, this.bodyY + 90, "monsterParts", leg); 
+        my.sprite.rightLeg.scaleX = 0.75; my.sprite.rightLeg.scaleY = 0.75;
+        my.sprite.leftLeg = this.add.sprite(this.bodyX - 70, this.bodyY + 90, "monsterParts", leg);
+        my.sprite.leftLeg.scaleX = 0.75; my.sprite.leftLeg.scaleY = 0.75;
+        my.sprite.leftLeg.flipX = true;
+        // mouth
+
+        // smile
+        my.sprite.smile = this.add.sprite(this.bodyX, this.bodyY + 50, "monsterParts", "mouth_closed_happy.png");
+        my.sprite.smile.setVisible(false);
+        // fangs
+        my.sprite.fangs = this.add.sprite(this.bodyX, this.bodyY + 50, "monsterParts", "mouthJ.png");
+
+        // S - smile 
+        // F - show fangs
+        // A - move left 
+        // D - move right
+        // move monster with this fcn
+        this.input.keyboard.on("keydown", function(event) {
+
+        });
     }
 
     update() {
